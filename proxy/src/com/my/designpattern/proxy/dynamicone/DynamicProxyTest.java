5892c56ee142303139c2294bd573b2392441feb0
mrzhu <2692027659@qq.com>
2018-07-27 17:19:56 +0800

    implements proxy pattern in java : static proxy; dynamic proxy using jdk utils, dynamic proxy using cglib ... ...

--- proxy/src/com/my/designpattern/proxy/dynamicone/DynamicProxyTest.java
+++ proxy/src/com/my/designpattern/proxy/dynamicone/DynamicProxyTest.java
@@ -5,14 +5,14 @@ import com.my.designpattern.proxy.Customer;
 public class DynamicProxyTest {
     public static void main(String[] args) {
         StudentCustomer studentCustomer = new StudentCustomer();
-        System.out.println("how many times :" + StudentCustomer.count);
+        System.out.println("how many times to call buyInsurance() : " + StudentCustomer.count);
 
         Customer customer = (Customer) new EducationInsuranceAgent().getInstance(studentCustomer);
         //System.out.println(customer.getClass());//class com.sun.proxy.$Proxy0
         customer.buyInsurance();
         customer.buyInsurance();
         //System.out.println(customer);//null ????????
-        System.out.println("how many times :" + StudentCustomer.count);//2
+        System.out.println("how many times to call buyInsurance() : " + StudentCustomer.count);//2
 
     }
 
